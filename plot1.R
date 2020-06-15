f5281251b546c0854f3a5e0ab58ab47e76a38acf
Phillip <58861772+pcart234@users.noreply.github.com>
2020-06-15 12:02:28 -0400

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,18 +1,23 @@
 library(dplyr)
 library(sqldf)
 library(lubridate)
-selecteddata <- read.csv.sql("C:/Users/zehra/Desktop/Explanatory Data Analysis/household_power_consumption.txt", 
-                             sql= "Select * from file where Date =='1/2/2007' OR Date =='2/2/2007'",
-                             header = TRUE, sep = ";")
+P_data1 <- read.table("household_power_consumption.txt",header = TRUE
+                    ,stringsAsFactors = FALSE, sep = ";", col.names = c("Date","Time" 
+                    ,"Global_active_power","Global_reactive_power","Voltage",
+                    "Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+                     skip=grep("1/2/2007", readLines("household_power_consumption.txt" )), 
+                     nrows=2878)
+                                                           
+head(P_data1)
+                                                       
+View(P_data1)
+                                                           
+hist(P_data1$Global_active_power, col = "red",
+xlab = "Global Active Power (kilowatts)", main = "Global Active Power")
 
-selecteddata <- mutate(selecteddata, DateTime = as.POSIXct(paste(Date, Time), format ="%d/%m/%Y %H:%M:%S"))
 
-hist(selecteddata$Global_active_power, col = "red",
-       xlab = "Global Active Power (kilowatts)", main = "Global Active Power")
+rug(P_data1$Global_active_power)
+abline(v= median(P_data1$Global_active_power), col="yellow", lwd=4)
 
-rug(selecteddata$Global_active_power)
-abline(v= median(selecteddata$Global_active_power), col="yellow", lwd=4)
 dev.copy(png, "plot1.png", width=480, height=480)
 dev.off()
-
-  
