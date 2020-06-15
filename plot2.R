f42159ecdbde2cde1940f4021ee2d7ea0d7c9017
Phillip <58861772+pcart234@users.noreply.github.com>
2020-06-15 12:29:14 -0400

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,15 +1,23 @@
 library(dplyr)
 library(sqldf)
 library(lubridate)
-selecteddata <- read.csv.sql("C:/Users/zehra/Desktop/Explanatory Data Analysis/household_power_consumption.txt", 
-                             sql= "Select * from file where Date =='1/2/2007' OR Date =='2/2/2007'",
-                             header = TRUE, sep = ";")
+P_data1 <- read.table("household_power_consumption.txt",header = TRUE
+                    ,stringsAsFactors = FALSE, sep = ";", col.names = c("Date","Time" 
+                                                           ,"Global_active_power","Global_reactive_power","Voltage",
+                                                           "Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3"),
+                                                           skip=grep("1/2/2007", readLines("household_power_consumption.txt" )), 
+                                                           nrows=2878)
+                                                           
+                                                           
 
-selecteddata <- mutate(selecteddata, DateTime = as.POSIXct(paste(Date, Time), format ="%d/%m/%Y %H:%M:%S"))
+P_data1 <- mutate(P_data1, DateTime = as.POSIXct(paste(Date, Time), format ="%d/%m/%Y %H:%M:%S"))
 
-plot(Global_active_power ~ DateTime, data = selecteddata, xlab = "", ylab = "Global Active Power (kilowatts)", 
-     type = "n")
-
-lines(Global_active_power ~ DateTime, data = selecteddata)
+plot(Global_active_power ~ DateTime, data = P_data1, xlab = "", ylab = "Global Active Power (kilowatts)", 
+type = "n")
+                                                           
+lines(Global_active_power ~ DateTime, data = P_data1)
+                                                           
+                                                           
 dev.copy(png, "plot2.png", width=480, height=480)
+                                                    
 dev.off()
